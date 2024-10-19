ed77eba8ed6fd1c8b9d8ccdf477ff0c5039307d8
rendellc <25772793+rendellc@users.noreply.github.com>
2024-10-19 15:28:46 +0200

    add alarm-state event parser

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.23.2
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.5.0
-	github.com/gorilla/mux v1.8.1
-	github.com/gorilla/websocket v1.5.3
 	github.com/graarh/golang-socketio v0.0.0-20170510162725-2c44953b9b5f
+	github.com/mitchellh/mapstructure v1.5.0
 	golang.org/x/oauth2 v0.23.0
 )
 
 require (
+	github.com/gorilla/websocket v1.5.3 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 )
--- homely/utils.go
+++ homely/utils.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"reflect"
+	"time"
+
+	"github.com/mitchellh/mapstructure"
 )
 
 func mapGet[V any](m map[string]any, keys ...string) (V, bool) {
@@ -93,8 +96,37 @@ func parseDeviceStateChangeEvent(eventAny any) (EventDeviceStateChanged, error) 
 	}, nil
 }
 
+// 2024/10/10 07:43:10 event-type: alarm-state-changed {"Data":{"Changes":null,"DeviceID":"","GatewayID":"","LocationID":"a34c83af-3ebe-433d-9f71-c6718c0fb647","ModelID":"","RootLocationID":""},"Type":"alarm-state-changed"}
+// 2024/10/10 16:42:03 event: {"data":{"locationId":"a34c83af-3ebe-433d-9f71-c6718c0fb647","partnerCode":1275,"state":"ARM_PENDING","timestamp":"2024-10-10T14:42:03.290Z"},"type":"alarm-state-changed"}
+// 2024/10/10 16:42:06 event: {"data":{"deviceId":"f3660d94-14cb-4909-b61f-04912a340917","eventId":594,"locationId":"a34c83af-3ebe-433d-9f71-c6718c0fb647","partnerCode":1275,"state":"DISARMED","timestamp":"2024-10-10T14:42:06.300Z","userId":"452e7b85-e94d-4742-8ff0-670a056ad908","userName":"Rendell Cale"},"type":"alarm-state-changed"}
+// 2024/10/10 07:43:10 event-type: alarm-state-changed
+// {"Data":{"Changes":null,"DeviceID":"","GatewayID":"","LocationID":"a34c83af-3ebe-433d-9f71-c6718c0fb647","ModelID":"","RootLocationID":""},"Type":"alarm-state-changed"}
+type alarmChangeData struct {
+	DeviceID    string    `json:"deviceId"`
+	EventID     string    `json:"eventId"`
+	LocationID  string    `json:"locationId"`
+	PartnerCode string    `json:"partnerCode"`
+	State       string    `json:"state"`
+	Timestamp   time.Time `json:"timestamp"`
+	UserID      string    `json:"userId"`
+	UserName    string    `json:"userName"`
+}
+
 func parseAlarmStateChangeEvent(eventAny any) (EventAlarmStateChanged, error) {
 	var invalid EventAlarmStateChanged
+	type alarmEvent struct {
+		Data alarmChangeData
+	}
+	event := alarmEvent{}
+	err := mapstructure.Decode(eventAny, &event)
+	if err != nil {
+		return invalid, fmt.Errorf("unable to decode %v as alarmEvent: %w", eventAny, err)
+	}
 
-	return invalid, fmt.Errorf("parse alarm change event not implemented")
+	return EventAlarmStateChanged{
+		DeviceID:  event.Data.DeviceID,
+		UserName:  event.Data.UserName,
+		State:     event.Data.State,
+		Timestamp: event.Data.Timestamp,
+	}, nil
 }
